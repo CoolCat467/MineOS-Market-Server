06044e500c9f8372e924a70dc3ec92ac5d2913f8
CoolCat467 <52022020+CoolCat467@users.noreply.github.com>
2024-08-08 00:06:33 -0500

    Update tests with new changes

--- tests/test_schema.py
+++ tests/test_schema.py
@@ -42,35 +42,32 @@ def test_parse_int(string: str | int, expect: int | None) -> None:
 @pytest.mark.parametrize(
     ("string", "expect"),
     [
-        ("-5", {}),
-        ("127=3", {"127": "3"}),
-        ("pi=3.14", {"pi": "3.14"}),
-        ("0xff", {}),
-        ("0=zero&1=one", {"0": "zero", "1": "one"}),
-        ("1=one&0=zero", {"0": "zero", "1": "one"}),
-        ("[1]=one&[0]=zero", {"": {"0": "zero", "1": "one"}}),
-        ("1[27]=one&0[6]=zero", {"0": {"6": "zero"}, "1": {"27": "one"}}),
+        ({"127": "3"}, {"127": "3"}),
+        ({"pi": "3.14"}, {"pi": "3.14"}),
+        ({"0": "zero", "1": "one"}, {"0": "zero", "1": "one"}),
+        ({"[1]": "one", "[0]": "zero"}, {"": {"0": "zero", "1": "one"}}),
+        (
+            {"1[27]": "one", "0[6]": "zero"},
+            {"0": {"6": "zero"}, "1": {"27": "one"}},
+        ),
     ],
 )
-def test_parse_table(string: str, expect: dict[str, Any]) -> None:
-    assert schema.parse_table(string) == expect
+def test_parse_table(string: dict[str, str], expect: dict[str, Any]) -> None:
+    assert schema.parse_table(string, None) == expect
 
 
 @pytest.mark.parametrize(
     ("string", "expect"),
     [
-        ("-5", []),
-        ("127=3", []),
-        ("pi=3.14", []),
-        ("0xff", []),
-        ("0=zero&1=one", []),
-        ("1=one&0=zero", []),
-        ("[1]=one&[0]=zero", []),
-        ("1[27]=one&0[6]=zero", []),
-        ("[27]=4&[6]=7", [4, 7]),  # sorted keys being strings
-        ("[3]=27&[1]=49", [49, 27]),
-        ("[0]=27&[1]=49", [27, 49]),
+        ({"127": "3"}, [3]),
+        ({"pi": "3.14"}, []),
+        ({"0": "zero", "1": "one"}, []),
+        ({"[1]": "one", "[0]": "zero"}, []),
+        ({"1[27]": "one", "0[6]": "zero"}, []),
+        ({"[27]": "4", "[6]": "7"}, [4, 7]),  # sorted keys being strings
+        ({"[3]": "27", "[1]": "49"}, [49, 27]),
+        ({"[0]": "27", "[1]": "49"}, [27, 49]),
     ],
 )
-def test_parse_int_list(string: str, expect: list[int]) -> None:
+def test_parse_int_list(string: dict[str, str], expect: list[int]) -> None:
     assert schema.parse_int_list(string) == expect
